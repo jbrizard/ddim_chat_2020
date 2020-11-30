0b06252f79ddeae2737727d47b018d98d767bedc
AffreuxLouis <47830978+AffreuxLouis@users.noreply.github.com>
2020-11-30 16:52:00 +0100

    meteo version 0.1

--- server/modules/meteo.js
+++ server/modules/meteo.js
@@ -4,3 +4,24 @@
  * Auteur(s) : Louis Glaunig
  */
 
+module.exports =  {
+	handleMeteo: handleMeteo // permet d'appeler cette méthode dans server.js -> daffy.handleDaffy(...)
+}
+
+/**
+ * Appel de la fonction météo dans le tchat
+ */
+function handleMeteo()
+{
+
+	// Est-ce qu'il contient une référence à Daffy ?
+	if (message.includes('meteo'))
+	{
+		// Si oui, envoie la réponse de Daffy...
+		io.sockets.emit('new_message',
+		{
+			name:'Daffy!!',
+			message:'<span class="daffy">Coin Coin !</span>'
+		});
+	}
+}
