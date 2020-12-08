3cbb01982414044fac3d211f503fe610ae14daa5
Eric Rondo <eric.rondo@gmail.com>
2020-12-08 14:31:06 +0100

    Ajout du module Donald

---
+++ server/modules/donald.js
@@ -0,0 +1,54 @@
+﻿/*
+ * Nom : Donald Bot
+ * Description : Un bot conversationnel
+ * Auteur(s) : Jérémie
+ */
+ 
+ // Définit les méthodes "publiques" (utilisation à l'extérieur du module)
+module.exports =  {
+	handleDonald: onNewMessage
+}
+
+var discussionEngaged = false;
+
+/**
+ * Fonction appelée lorsqu'on reçoit un nouveau message
+ */
+function onNewMessage(io, message)
+{
+	console.log(message);
+	
+	if (message.toLowerCase().includes('hey donald'))
+	{
+		sendMessage(io, 'Hey, tu m\'as parlé ?');
+		discussionEngaged = true;
+	}
+	
+	if (discussionEngaged && message.toLowerCase().includes('comment &#231;a va?'))
+	{
+		sendMessage(io, 'Tout baigne et toi ?!');
+	}
+	
+	if (discussionEngaged && message.toLowerCase().includes('comment se passent les &#233;lections?'))
+	{
+		sendMessage(io, '<img class="donald-gif" src="/modules/donald/good.gif">');
+	}
+	
+	if (discussionEngaged && message.toLowerCase().includes('bye donald'))
+	{
+		sendMessage(io, 'A+ gros');
+		discussionEngaged = false;
+	}
+}
+
+function sendMessage(io, message)
+{
+	setTimeout(function()
+	{
+			io.sockets.emit('new_message',
+			{
+				name:'Donald',
+				message: message
+			});
+	}, 700);
+}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var fs = require('fs');			// Accès au système de fichier
 
 // Chargement des modules perso
 var daffy = require('./modules/daffy.js');
+var donald = require('./modules/donald.js');
 
 // Initialisation du serveur HTTP
 var app = express();
@@ -46,6 +47,9 @@ io.sockets.on('connection', function(socket)
 		
 		// Transmet le message au module Daffy (on lui passe aussi l'objet "io" pour qu'il puisse envoyer des messages)
 		daffy.handleDaffy(io, message);
+		
+		// Transmet le message au module Donald (on lui passe aussi l'objet "io" pour qu'il puisse envoyer des messages)
+		donald.handleDonald(io, message);
 	});
 });
 
