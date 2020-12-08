9e366b0eab1d0847d6544504e2d0b4b50df2160d
Louise <louise.m.bertin@gmail.com>
2020-12-08 14:55:53 +0100

    Ajout nom, description, auteurs, modification commentaires

--- server/modules/wiki.js
+++ server/modules/wiki.js
@@ -1,30 +1,30 @@
 ﻿/*
- * Nom : Daffy !
- * Description : Ce module ne fait pas grand chose... quand on appelle Daffy, il répond !
- * Auteur(s) : Jérémie Brizard
+ * Nom : Wiki !
+ * Description : Renvoie la page wikipédia du mot demandé
+ * Auteurs : Louise Bertin & Quentin Pionnier
  */
 
 // Définit les méthodes "publiques" (utilisation à l'extérieur du module)
 module.exports =  {
-	handleWiki: handleWiki // permet d'appeler cette méthode dans server.js -> daffy.handleDaffy(...)
+	handleWiki: handleWiki // permet d'appeler cette méthode dans server.js -> wiki.handleWiki(...)
 }
 
 /**
- * Lorsqu'on appelle Daffy, il répond...
+ * Lorsqu'on appelle wiki, il répond...
  */
 function handleWiki(io, message)
 {
 	// Passe le message en minuscules (recherche insensible à la casse)
 	message = message.toLowerCase();
 	
-	// Est-ce qu'il contient une référence à Daffy ?
-	if (message.includes('wiki'))
+	// Est-ce qu'il contient une référence à wiki ?
+	if (message.includes('wiki') && message.includes('wikipedia'))
 	{
-		// Si oui, envoie la réponse de Daffy...
+		// Si oui, envoie la réponse de wiki...
 		io.sockets.emit('new_message',
 		{
 			name:'Wiki',
-			message:'<span class="wiki">ça arrive, patiente</span>'
+			message:'<span class="wiki">Ca arrive, patience...</span>'
 		});
 	}
 }
